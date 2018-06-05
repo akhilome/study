648dec0b2b2b01c12acdac7af071a5d01e69a5e4
akhilome <kizito@akhilo.me>
2018-06-05 23:00:52 +0100

    update(codewars): add new kata

--- challenges/codeWars.js
+++ challenges/codeWars.js
@@ -188,3 +188,29 @@ function isPrime(num) {
   
   return true;
 }
+
+
+/* 
+	#5: 06/05/2018
+
+	If we list all the natural numbers below 10 that are multiples of 3 
+	or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+	Finish the solution so that it returns the sum of all the multiples 
+	of 3 or 5 below the number passed in.
+
+	Note: If the number is a multiple of both 3 and 5, only count it once.
+
+*/
+
+// My Solution:
+
+const solution = number => {
+  let sum = 0;
+
+  for(let i = 0; i < number; i++) {
+    sum += !(i % 3) && !(i % 5) ? i : !(i % 3) ? i : !(i % 5) ? i : 0;
+  }
+
+  return sum;
+}
