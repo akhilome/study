d9b8694104f2df056985f50602bf46cf133c63a6
akhilome <kizito@akhilo.me>
2018-06-06 16:37:45 +0100

    update(codewars): add new kata

--- challenges/codeWars.js
+++ challenges/codeWars.js
@@ -214,3 +214,32 @@ const solution = number => {
 
   return sum;
 }
+
+/*
+	#6: 06/06/2018
+
+	Write a function toWeirdCase (weirdcase in Ruby) that accepts a string,
+	and returns the same string with all even indexed characters in each 
+	word upper cased, and all odd indexed characters in each word lower 
+	cased. The indexing just explained is zero based, so the zero-ith 
+	index is even, therefore that character should be upper cased.
+
+	The passed in string will only consist of alphabetical characters and 
+	spaces(' '). Spaces will only be present if there are multiple words. 
+	Words will be separated by a single space(' ').
+
+*/
+
+// My Solution:
+
+function toWeirdCase(string){
+  const weirdoMaker = word =>  {
+    let strArr = word.split('');
+    strArr.forEach((char, i, arr) => {
+        arr[i] = i % 2 ? char.toLowerCase() : char.toUpperCase();
+    });
+    return strArr.join('');
+  }
+  
+  return string.includes(" ") ? string.split(" ").map(weirdoMaker).join(" ") : weirdoMaker(string);
+}
