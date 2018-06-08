5f5ea58ce08651d7d09a4214c26e8e5e1038d64d
akhilome <kizito@akhilo.me>
2018-06-08 21:30:51 +0100

    update(codewars): add new kata

--- challenges/codeWars.js
+++ challenges/codeWars.js
@@ -276,3 +276,67 @@ const moveZeros = arr => {
 const moveZeros = arr => {
 	return arr.filter(char => char !== 0).concat(arr.filter(char => char === 0));
 }
+
+
+/*
+	#8: 06/08/2018
+
+	This time we want to write calculations using functions and 
+	get the results. Let's have a look at some examples:
+
+		even(times(five())); // must return 35
+		four(plus(nine())); // must return 13
+		eight(minus(three())); // must return 5
+		six(dividedBy(two())); // must return 3
+
+	Requirements:
+
+		- There must be a function for each number from 0 ("zero") to 
+		9 ("nine")
+
+		- There must be a function for each of the following mathematical 
+		operations: plus, minus, times, dividedBy (divided_by in Ruby)
+		
+		- Each calculation consist of exactly one operation and two 
+		numbers
+		
+		- The most outer function represents the left operand, the 
+		most inner function represents the right operand
+		
+		- Divison should be integer division
+*/
+
+// My solution: 
+
+const checker = (operation, right, left) => {
+  switch (operation) {
+    case 'mult':
+      return left * right;
+      break;
+    case 'div':
+      return Math.floor(left / right);
+      break;
+    case 'plus':
+      return left + right;
+      break;
+    case 'minus':
+      return left - right;
+      break;
+  }
+}
+
+function zero(arg) {return arg === undefined ? 0 : checker(arg[0], arg[1], 0)}
+function one(arg) {return arg === undefined ? 1 : checker(arg[0], arg[1], 1)}
+function two(arg) {return arg === undefined ? 2 : checker(arg[0], arg[1], 2)}
+function three(arg) {return arg === undefined ? 3 : checker(arg[0], arg[1], 3)}
+function four(arg) {return arg === undefined ? 4 : checker(arg[0], arg[1], 4)}
+function five(arg) {return arg === undefined ? 5 : checker(arg[0], arg[1], 5)}
+function six(arg) {return arg === undefined ? 6 : checker(arg[0], arg[1], 6)}
+function seven(arg) {return arg === undefined ? 7 : checker(arg[0], arg[1], 7)}
+function eight(arg) {return arg === undefined ? 8 : checker(arg[0], arg[1], 8)}
+function nine(arg) {return arg === undefined ? 9 : checker(arg[0], arg[1], 9)}
+
+function plus(x) { return ['plus', x] }
+function minus(x) { return ['minus', x] }
+function times(x) { return ['mult', x] }
+function dividedBy(x) { return ['div', x] }
