b8169b565f2b161ec304125d7228406d6aae4e2a
akhilome <kizito@akhilo.me>
2018-06-08 23:45:28 +0100

    refactor(codewars): rewrite previous kata
    Turns using the break keyword after a return statement is redundant

--- challenges/codeWars.js
+++ challenges/codeWars.js
@@ -366,19 +366,10 @@ function dividedBy(x) { return ['div', x] }
 
 function likes(names) {
   switch(names.length) {
-    case 0:
-      return "no one likes this";
-      break;
-    case 1:
-      return `${names[0]} likes this`;
-      break;
-    case 2:
-      return `${names[0]} and ${names[1]} like this`;
-      break;
-    case 3:
-      return `${names[0]}, ${names[1]} and ${names[2]} like this`;
-      break;
-    default:
-      return  `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+    case 0: return "no one likes this";
+    case 1: return `${names[0]} likes this`;
+    case 2: return `${names[0]} and ${names[1]} like this`;
+    case 3: return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+    default: return  `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
   }
 }
