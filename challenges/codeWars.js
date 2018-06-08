c97cf32f26111196a51c7753c82796670bdf679c
akhilome <kizito@akhilo.me>
2018-06-08 23:34:56 +0100

    update(codewars): add new kata

--- challenges/codeWars.js
+++ challenges/codeWars.js
@@ -340,3 +340,45 @@ function plus(x) { return ['plus', x] }
 function minus(x) { return ['minus', x] }
 function times(x) { return ['mult', x] }
 function dividedBy(x) { return ['div', x] }
+
+/* 
+	#9: 06/08/2018
+
+	You probably know the "like" system from Facebook and other 
+	pages. People can "like" blog posts, pictures or other items. 
+	We want to create the text that should be displayed next to 
+	such an item.
+
+	Implement a function likes :: [String] -> String, which must 
+	take in input array, containing the names of people who like an 
+	item. It must return the display text as shown in the examples:
+
+		likes [] // must be "no one likes this"
+		likes ["Peter"] // must be "Peter likes this"
+		likes ["Jacob", "Alex"] // must be "Jacob and Alex like this"
+		likes ["Max", "John", "Mark"] // must be "Max, John and Mark like this"
+		likes ["Alex", "Jacob", "Mark", "Max"] // must be "Alex, Jacob and 2 others like this"
+
+	For 4 or more names, the number in and 2 others simply increases.
+*/
+
+// My solution:
+
+function likes(names) {
+  switch(names.length) {
+    case 0:
+      return "no one likes this";
+      break;
+    case 1:
+      return `${names[0]} likes this`;
+      break;
+    case 2:
+      return `${names[0]} and ${names[1]} like this`;
+      break;
+    case 3:
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+      break;
+    default:
+      return  `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+  }
+}
