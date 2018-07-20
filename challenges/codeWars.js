cf590845b021be46b55245d2dba3d4e5d2986d28
Kizito Akhilome <kizito@akhilo.me>
2018-07-20 16:21:47 +0100

    update(codewars): Add new kata

--- challenges/codeWars.js
+++ challenges/codeWars.js
@@ -623,3 +623,26 @@ const scramble = (str1, str2) => {
 
   return true;
 }
+
+/*
+	#16: 07/20/2018
+
+	Write a function that will find all the anagrams of a word from a 
+	list. You will be given two inputs a word and an array with words. 
+	You should return an array of all the anagrams or an empty array 
+	if there are none. For example:
+
+	anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
+
+	anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', 'racer']
+
+	anagrams('laser', ['lazing', 'lazy',  'lacer']) => []
+
+*/
+
+// My Solution:
+
+const anagrams = (word, words) => {
+  const target = word.split('').sort().join('');
+  return words.filter(word => word.split('').sort().join('') === target);
+}
