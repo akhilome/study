5e6135ea3f6d3d772765a887711d48f0f7b5117e
Kizito Akhilome <hovkard@gmail.com>
2019-04-20 17:35:08 +0100

    update(gatsby-bootcamp): implement a layout container component
    - create a <Layout /> component to house component which would
    be used multiple times inside pages

--- courses/great-gatsby-bootcamp/gatsby-bootcamp/src/pages/blog.js
+++ courses/great-gatsby-bootcamp/gatsby-bootcamp/src/pages/blog.js
@@ -1,14 +1,13 @@
 import React from "react"
-import Footer from "../components/footer"
-import Header from "../components/header"
+import Layout from "../components/layout"
 
 const BlogPage = () => {
   return (
     <div>
-      <Header />
-      <h1>Blog Page</h1>
-      <p>Blog posts will show up here</p>
-      <Footer />
+      <Layout>
+        <h1>Blog Page</h1>
+        <p>Blog posts will show up here</p>
+      </Layout>
     </div>
   )
 }
