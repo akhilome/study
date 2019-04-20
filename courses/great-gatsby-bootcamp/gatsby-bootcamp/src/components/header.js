69caa4411c07bc9e90c169cab980bff668ecab12
Kizito Akhilome <hovkard@gmail.com>
2019-04-20 18:13:43 +0100

    update(gatsby-bootcamp): learn about css modules & style site

--- courses/great-gatsby-bootcamp/gatsby-bootcamp/src/components/header.js
+++ courses/great-gatsby-bootcamp/gatsby-bootcamp/src/components/header.js
@@ -1,22 +1,47 @@
 import React from "react"
 import { Link } from "gatsby"
+import headerStyles from "./header.module.scss"
 
 const Header = () => {
   return (
-    <header>
+    <header className={headerStyles.header}>
       <h1>
-        <Link to="/">Kizito's Place</Link>
+        <Link
+          to="/"
+          className={headerStyles.title}
+          activeClassName={headerStyles.activeNavItem}
+        >
+          Kizito's Place
+        </Link>
       </h1>
       <nav>
-        <ul>
+        <ul className={headerStyles.navList}>
           <li>
-            <Link to="/blog">blog</Link>
+            <Link
+              to="/blog"
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+            >
+              blog
+            </Link>
           </li>
           <li>
-            <Link to="/about">about</Link>
+            <Link
+              to="/about"
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+            >
+              about
+            </Link>
           </li>
           <li>
-            <Link to="/contact">contact</Link>
+            <Link
+              to="/contact"
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+            >
+              contact
+            </Link>
           </li>
         </ul>
       </nav>
--- courses/great-gatsby-bootcamp/gatsby-bootcamp/src/components/layout.js
+++ courses/great-gatsby-bootcamp/gatsby-bootcamp/src/components/layout.js
@@ -2,12 +2,15 @@ import React from "react"
 import Header from "./header"
 import Footer from "./footer"
 import "../styles/index.scss"
+import layoutStyles from "./layout.module.scss"
 
 const Layout = ({ children }) => {
   return (
-    <div>
-      <Header />
-      {children}
+    <div className={layoutStyles.container}>
+      <div className={layoutStyles.content}>
+        <Header />
+        {children}
+      </div>
       <Footer />
     </div>
   )
