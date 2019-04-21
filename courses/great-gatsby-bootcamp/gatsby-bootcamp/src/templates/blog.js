8bd64fea40d6e76a937da8aa56edfd9658a7d2b1
Kizito Akhilome <hovkard@gmail.com>
2019-04-21 05:25:29 +0100

    update(gatsby-bootcamp): add post template & gatsby creatPages config
    - add a boilerplate individual blog post template file
    - modify graphql query within blog to include post slug
    - add createPages config in gatsby-node config and create new page for each edge

---
+++ courses/great-gatsby-bootcamp/gatsby-bootcamp/src/templates/blog.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const Blog = () => {
+  return (
+    <Layout>
+      <p>This is the template for the blog</p>
+    </Layout>
+  )
+}
+
+export default Blog
