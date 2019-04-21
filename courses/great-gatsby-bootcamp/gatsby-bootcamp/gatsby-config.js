cde5fd937d2bdec97bb353d185f67e85d9d0e05b
Kizito Akhilome <hovkard@gmail.com>
2019-04-21 04:05:11 +0100

    update(gatsby-bootcamp): learn how to source data from fs
    - install the gatsby-source-filesystem plugin

--- courses/great-gatsby-bootcamp/gatsby-bootcamp/gatsby-config.js
+++ courses/great-gatsby-bootcamp/gatsby-bootcamp/gatsby-config.js
@@ -3,5 +3,14 @@ module.exports = {
     title: "Kizito's Place",
     author: "Kizito Akhilome",
   },
-  plugins: ["gatsby-plugin-sass"],
+  plugins: [
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "src",
+        path: `${__dirname}/src/`,
+      },
+    },
+  ],
 }
