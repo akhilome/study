cb5d6120f3b3e585e67e7a18aa5f5e7d4c2a73e4
Kizito Akhilome <hovkard@gmail.com>
2019-04-21 05:47:05 +0100

    update(gatsby-bootcamp): learn how to make dynamic graphql queries
    - modify blog post template to source content using graphql

--- courses/great-gatsby-bootcamp/gatsby-bootcamp/src/templates/blog.js
+++ courses/great-gatsby-bootcamp/gatsby-bootcamp/src/templates/blog.js
@@ -1,10 +1,29 @@
 import React from "react"
 import Layout from "../components/layout"
+import { graphql } from "gatsby"
 
-const Blog = () => {
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        date
+      }
+      html
+    }
+  }
+`
+
+const Blog = ({ data: { markdownRemark } }) => {
+  const {
+    frontmatter: { title, date },
+    html: __html,
+  } = markdownRemark
   return (
     <Layout>
-      <p>This is the template for the blog</p>
+      <h1>{title}</h1>
+      <p>{date}</p>
+      <div dangerouslySetInnerHTML={{ __html }} />
     </Layout>
   )
 }
