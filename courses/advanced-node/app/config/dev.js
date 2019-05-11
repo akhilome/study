2a50f588a407161a35c92d5f4dae84c1706cc7c1
Kizito Akhilome <hovkard@gmail.com>
2019-05-11 22:45:49 +0100

    update(advanced-node): provision mongodb instance

--- courses/advanced-node/app/config/dev.js
+++ courses/advanced-node/app/config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://kizito:passw0rd@ds155396.mlab.com:55396/blog_dev',
   cookieKey: '123123123'
 };
