2a15b1c3b1e59502138e55750e1c0db6f218ee13
Kizito Akhilome <hovkard@gmail.com>
2019-07-27 20:40:21 +0100

    update(advanced-node): implement caching for all queries in app

---
+++ courses/advanced-node/app/services/cache.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
+
+const redisUrl = 'redis://localhost:6379';
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
+
+const exec = mongoose.Query.prototype.exec;
+
+mongoose.Query.prototype.exec = async function() {
+  const key = JSON.stringify({
+    ...this.getQuery(),
+    collection: this.mongooseCollection.name
+  });
+
+  const cacheValue = await client.get(key);
+
+  if (cacheValue) {
+    const doc = JSON.parse(cacheValue);
+
+    return Array.isArray(doc)
+      ? doc.map(d => new this.model(d))
+      : new this.model(doc);
+  }
+
+  const result = await exec.apply(this, arguments);
+
+  client.set(key, JSON.stringify(result));
+
+  return result;
+};
