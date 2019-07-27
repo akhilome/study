115d657bab048b6f276f07d5fd8f2cd96944cf6d
Kizito Akhilome <hovkard@gmail.com>
2019-07-27 20:58:43 +0100

    update(advanced-node): refactor caching strategy to use hashes
    - blogposts should be cached under each user id namespace

--- courses/advanced-node/app/services/cache.js
+++ courses/advanced-node/app/services/cache.js
@@ -4,10 +4,11 @@ const util = require('util');
 
 const redisUrl = 'redis://localhost:6379';
 const client = redis.createClient(redisUrl);
-client.get = util.promisify(client.get);
+client.hget = util.promisify(client.hget);
 
 const exec = mongoose.Query.prototype.exec;
-mongoose.Query.prototype.cache = function() {
+mongoose.Query.prototype.cache = function(options) {
+  this.cacheKey = JSON.stringify(options.key) || '';
   this.useCache = true;
   return this;
 };
@@ -20,7 +21,7 @@ mongoose.Query.prototype.exec = async function() {
     collection: this.mongooseCollection.name
   });
 
-  const cacheValue = await client.get(key);
+  const cacheValue = await client.hget(this.cacheKey, key);
 
   if (cacheValue) {
     const doc = JSON.parse(cacheValue);
@@ -32,7 +33,7 @@ mongoose.Query.prototype.exec = async function() {
 
   const result = await exec.apply(this, arguments);
 
-  client.set(key, JSON.stringify(result));
+  client.hset(this.cacheKey, key, JSON.stringify(result));
 
   return result;
 };
