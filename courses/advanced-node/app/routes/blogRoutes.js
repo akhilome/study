230d46c1edfc50b1fcd1e34d927f64ff4ae376fd
Kizito Akhilome <hovkard@gmail.com>
2019-07-27 20:45:19 +0100

    update(advanced-node): make caching optional
    - only queries which call the custom .cache() method would get
    cached to the redis instance

--- courses/advanced-node/app/routes/blogRoutes.js
+++ courses/advanced-node/app/routes/blogRoutes.js
@@ -14,8 +14,7 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
-
+    const blogs = await Blog.find({ _user: req.user.id }).cache();
     res.send(blogs);
   });
 
--- courses/advanced-node/app/services/cache.js
+++ courses/advanced-node/app/services/cache.js
@@ -7,8 +7,14 @@ const client = redis.createClient(redisUrl);
 client.get = util.promisify(client.get);
 
 const exec = mongoose.Query.prototype.exec;
+mongoose.Query.prototype.cache = function() {
+  this.useCache = true;
+  return this;
+};
 
 mongoose.Query.prototype.exec = async function() {
+  if (!this.useCache) return exec.apply(this, arguments);
+
   const key = JSON.stringify({
     ...this.getQuery(),
     collection: this.mongooseCollection.name
